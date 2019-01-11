114f210438fee9aed095d9b4e323698e85c759a1
KanatBa <kbaialiev@gmail.com>
2019-01-11 01:35:28 -0600

    first

--- CyberBank/Configuration/confs1.js
+++ CyberBank/Configuration/confs1.js
@@ -44,9 +44,15 @@ sauceKey: '405a565a-c90e-49d9-ab5e-a49bc94dde4f',
        }).getJasmine2Reporter());
 
        var AllureReporter = require('jasmine-allure-reporter');
-       jasmine.getEnv().addReporter(new AllureReporter({
-         resultsDir: 'allure-results'
-       }));
+     jasmine.getEnv().addReporter(new AllureReporter());
+     jasmine.getEnv().afterEach(function(done){
+       browser.takeScreenshot().then(function (png) {
+         allure.createAttachment('Screenshot', function () {
+           return new Buffer(png, 'base64')
+         }, 'image/png')();
+         done();
+       })
+     });
  
       },
          
--- CyberBank/Configuration/grid.js
+++ CyberBank/Configuration/grid.js
@@ -36,9 +36,15 @@ exports.config = {
        }).getJasmine2Reporter());
 
        var AllureReporter = require('jasmine-allure-reporter');
-       jasmine.getEnv().addReporter(new AllureReporter({
-         resultsDir: 'allure-results'
-       }));
+     jasmine.getEnv().addReporter(new AllureReporter());
+     jasmine.getEnv().afterEach(function(done){
+       browser.takeScreenshot().then(function (png) {
+         allure.createAttachment('Screenshot', function () {
+           return new Buffer(png, 'base64')
+         }, 'image/png')();
+         done();
+       })
+     });
  
       },
            
